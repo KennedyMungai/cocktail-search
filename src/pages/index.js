ac9c03bb8402efddaf6838b92cb0f997135e8525
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-31 03:11:01 +0300

    Created the Home component

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () =>
+{
+    return (
+        <div>
+            {/* TODO Add the relevant components */}
+            {/* Search input */}
+            {/* Cocktail List */}
+        </div> >
+    )
+}
+
+export default Home
