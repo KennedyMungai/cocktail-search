b2101aaa53b5fca4832c50e0624ace3b9d98488f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-31 04:38:20 +0300

    Created and exported as the default the confgureSTore for teh reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,2 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cocktailReducer from "./features/cocktailSlice";
+
+
+export default configureStore({
+    reducer: {
+        app: cocktailReducer
+    }
+})
