3c84735990fc90d1754654a36f2c98448d92a910
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-31 04:40:18 +0300

    Set up the provider and wired up the store

--- src/index.js
+++ src/index.js
@@ -11,8 +11,10 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
