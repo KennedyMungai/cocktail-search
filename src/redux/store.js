9d80ab4277a389c24600e58a358253d7663b9bec
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-31 04:25:05 +0300

    Created the fetchCocktails asunc thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+
+export const fetchCocktails = createAsyncThunk("cocktails/fetchCocktails", async () => 
+{
+    return fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=").then((res) => res.json())
+})
