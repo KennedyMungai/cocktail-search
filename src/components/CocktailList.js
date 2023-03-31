4aa391baf3face4c674c8cf02d14313e5b5e60a2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-31 04:59:06 +0300

    Imported Link from react-router-dom

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchCocktails } from '../redux/features/cocktailSlice'
+import { Link } from 'react-router-dom'
 
 
 
