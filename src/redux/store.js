fc489ac8629b64d52530ef3cd6186bc6c3ea49dd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-31 04:30:08 +0300

    Wrote , in part, the logoc of the cocktailSlice state

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,3 +5,24 @@ export const fetchCocktails = createAsyncThunk("cocktails/fetchCocktails", async
 {
     return fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=").then((res) => res.json())
 })
+
+
+const cocktailSlice = createSlice({
+    name: "cocktails",
+    initialState: {
+        cocktails: [],
+        cocktail: [],
+        loading: false,
+        error: null
+    },
+    extraReducers: {
+        [fetchCocktails.pending]: (state, action) => 
+        {
+            state.loading = true
+        },
+        [fetchCocktails.fulfilled]: (state, action) =>
+        {
+            state.cocktails = action.payload.drinks
+        }
+    }
+})
