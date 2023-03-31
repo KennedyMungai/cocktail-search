379baccaae2fcd6503a0c1fd6dfd174024d7236f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-31 04:53:49 +0300

    Used searchValue as the ref for the input tag

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -4,12 +4,15 @@ import './SearchInput.css'
 
 const SearchInput = () =>
 {
+    const searchValue = useRef()
+
+
     return (
         <section className="section search">
             <form action="" className="search-form">
                 <div className="form-control">
                     <label htmlFor="name">Search Cocktail</label>
-                    <input type="text" id="name" name='name' />
+                    <input type="text" id="name" name='name' ref={searchValue} />
                 </div>
             </form>
         </section>
