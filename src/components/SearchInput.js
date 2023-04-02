210e3d73d27bc3d7c70871b3533a076fac585781
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-02 09:51:11 +0300

    Created an instance of useDispatch called dispatch

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -7,6 +7,7 @@ import { useDispatch } from 'react-redux'
 const SearchInput = () =>
 {
     const searchValue = useRef()
+    const dispatch = useDispatch()
 
     const handleSubmit = (e) =>
     {
